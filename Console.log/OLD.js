f2aec3750159560f2ad3ad99cd9a505f7c5a432a
Mubin <fathummubin26@gmail.com>
2026-01-11 03:22:57 +0600

    Update backend and frontend files

---
+++ Console.log/OLD.js
@@ -0,0 +1,53 @@
+let x=55;
+console.log(`hi my age is ${x}`); //console.log(``) = printf
+console.log(`hi my age is ${x}`);
+console.log(`hi my age is ${x}`);
+let food = "Rice";
+console.log(`${food} is good for health` ); //${food} = variables
+
+let y = "robot"
+console.log(`${y} is good`);
+let fullname = "FATHUM MUBIN"; // Added let for safety in standalone file
+console.log(   `The name is ${fullname}`);
+
+let z = 55;
+ui = z -1;
+console.log(`${ui}`);
+
+uii= ui**3;  //power  
+console.log(`${uii}`);
+
+let A = 31
+a= A%6; //modulas
+console.log(`${a}`); 
+
+let result = 1+2+3**5/34-35 ;
+console.log(    `${result}`);
+
+
+//let username;
+// username = window.prompt("why man"); 
+//console.log(username);
+
+let birthday = 25;
+birthday = birthday + 1;
+console.log(`birthday`);
+
+let m = "0";
+let n = "jole";
+let o = "";
+
+m =Number(m);
+n =String(n);
+o =Boolean(o);
+
+    console.log(m,typeof m); 
+     console.log(n,typeof n);
+      console.log(o,typeof 0);
+const pi = 3.14169;
+let radius ;
+let C;
+//radius = window.prompt("enter the radious");
+radius= Number(radius);
+C = 2 * pi * radius;
+console.log(C);
