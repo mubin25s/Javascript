40a4147234f19652f51d15b917db55816d17927b
Mubin <fathummubin26@gmail.com>
2026-01-22 00:46:11 +0600

    Update backend and frontend files

--- Document.getElementBy/class_event.js
+++ Document.getElementBy/class_event.js
@@ -5,5 +5,3 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function() {
         alert("Button " + (i+1) + " was clicked!");
         this.style.backgroundColor = "lightgreen";
-    }
-}
