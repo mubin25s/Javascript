1144e784df493696c07ef9f0a334f8e9b8118839
Mubin <fathummubin26@gmail.com>
2026-02-15 00:46:20 +0600

    Update backend and frontend files

---
+++ TypeScript/Simple Problems/generics_basics.ts
@@ -0,0 +1,30 @@
+// Generics Basics
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let output1 = identity<string>("myString");
+let output2 = identity<number>(100);
+
+console.log(`String Output: ${output1}`);
+console.log(`Number Output: ${output2}`);
+
+// Generic Interface
+interface Box<T> {
+    content: T;
+}
+
+let box: Box<number> = { content: 50 };
+console.log(`Box Content: ${box.content}`);
+
+// Generic Class
+class GenericNumber<T> {
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = (x, y) => x + y;
+
+console.log(`Generic Addition: ${myGenericNumber.add(10, 20)}`);
